7fab3e4097b5950bcd4653a2aa87a8d8d0eecb38
rukundo <rukundowilson5@gmail.com>
2025-03-29 09:29:34 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   server: {
     proxy: {
-      '/api': 'http://localhost:8080',
+      '/api': {
+        target: 'https://ziggycom-backend.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
     },
-    host: true,
-    port: 5173,
-    strictPort: true,
-    allowedHosts: ["2129-197-157-184-238.ngrok-free.app"]
-  }
-})
+  },
+});
