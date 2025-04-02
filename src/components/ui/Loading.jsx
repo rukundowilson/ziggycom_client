ac32714968568f19b1719c9123683e4235e5ca13
rukundo <rukundowilson5@gmail.com>
2025-04-02 16:52:23 +0000

    ...

--- src/components/ui/Loading.jsx
+++ src/components/ui/Loading.jsx
@@ -1,8 +1,10 @@
 const Loading= () => {
     return (
         <div className="flex items-center justify-center h-64">
-            <div className="text-gray-500 text-lg">be patient, we are logging you to ziggycom</div>
-            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
+            <div>
+                <div className="text-gray-500 text-lg">be patient, we are logging you to ziggycom</div>
+                <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
+            </div>
             <div className="text-gray-700 mt-4">Loading...</div>
         </div>
     );
