9c5485f051e61f561f28d80329c14bf46e59da7b
rukundo <rukundowilson5@gmail.com>
2025-04-04 16:51:30 +0000

    ..

--- src/components/ui/LoadingComponent.jsx
+++ src/components/ui/LoadingComponent.jsx
@@ -1,6 +1,6 @@
 const LoadingState= () => {
     return (
-        <div className="flex items-center justify-center h-64">
+        <div className="flex items-center my-20 justify-center h-64">
             <div className="text-center">
                 <div className="block mx-auto animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
                 <div className="text-gray-700 mt-4">Loading...</div>
